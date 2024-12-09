72898c052dbd9f220385d8b03c807a37aa28d1e2
Amal Santhosh <amalsanthosh1403@gmail.com>
2024-12-09 16:02:06 +0530

    Completed CRUD Operations in Function Based Views

--- for_crud_operations/views.py
+++ for_crud_operations/views.py
@@ -5,7 +5,7 @@ from .serializers import *
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def StudentFunctionBasedAPI(request):
     if request.method == 'GET':
         entered_data = request.data    #enter_data will be in python format(dictionary)
@@ -34,4 +34,34 @@ def StudentFunctionBasedAPI(request):
             return Response(stu_seri.data)
         else:
             return Response(stu_seri.errors)
-        
+
+    elif request.method == 'PUT':
+        entered_data = request.data
+        entered_data_id = entered_data.get('id', None)
+
+        if entered_data_id is not None:
+            try:
+                stu_obj = Students.objects.get(id = entered_data_id)
+            except:
+                return Response({"error": "ID is not available for updating data"}, status=400)
+            stu_seri = StudentSerializer(stu_obj, data = entered_data, partial = True)
+            if stu_seri.is_valid():
+                stu_seri.save()
+                return Response(stu_seri.data)
+            return Response(stu_seri.errors)
+        else:
+            return Response({"error": "ID is required for updating data"}, status=400)
+            
+    elif request.method == "DELETE":
+        entered_data = request.data
+        entered_data_id = entered_data.get('id', None)
+
+        if entered_data_id is not None:
+            try:
+                stu_obj = Students.objects.get(id = entered_data_id)
+            except:
+                return Response({"error": "ID is not available for deleting data"})
+            stu_obj.delete()
+            return Response({"msg": "Data has been Deleted"})
+        return Response({"error": "ID is required for deleting data"}, status=400)
+
